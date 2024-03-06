b7a62dc218284feb4ee2f2f7be9106d5440f8c31
ssk042 <130318981+ssk042@users.noreply.github.com>
2024-03-06 15:47:16 -0800

    Update Environment.java
    variables in environment added

--- TestEpidemiology/src/TestEpidemiology/Environment.java
+++ TestEpidemiology/src/TestEpidemiology/Environment.java
@@ -6,4 +6,20 @@ public class Environment {
 		// TODO Auto-generated constructor stub
 	}
 
+	int gridWidth = 100;
+	int gridHeight = 100;
+	in searchRadius;
+	int numAgents;
+	int recoveryRate;
+	int recoveryError = 5;
+	double complianceAvg;
+	double complianceSD;
+	int burninTime;
+	double baseInfectionRate;
+	double randMove = 0.3;
+	boolean shareSpace = true;
+
+	ENUM status = (susceptible, exposed, infected, recovered);
+	clock = 0;
+
 }
