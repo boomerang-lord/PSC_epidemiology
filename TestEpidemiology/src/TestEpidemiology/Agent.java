3d65fda1211e5708e777e4b19afed7aedd06927f
ssk042 <130318981+ssk042@users.noreply.github.com>
2024-03-06 15:35:05 -0800

    Update Agent.java

--- TestEpidemiology/src/TestEpidemiology/Agent.java
+++ TestEpidemiology/src/TestEpidemiology/Agent.java
@@ -36,3 +36,35 @@ public class Agent implements Steppable {
 		
 	}
 }
+
+public bool checkQuarantine(environment_state) {
+	if (random() this.compliance) {
+		this.inQuarantine == true;
+	return true;
+	}
+}
+
+public bool checkRecover(environment_state) {
+	int recoveryTime = state.recoveryTime + nextInt(2 * state.recoveryError) - state.recoveryError;
+	if (this.sickime == recoveryTime) {
+			this.status == "RECOVERED";
+			this.inQuarantine == False;
+			return true;
+	return false;
+	}
+}
+
+public interact (Environment state, Bag neighbors) {
+	if(neighbors.isEmpty()) {
+		return;
+	}
+	for (agents i in neighbors) {
+		boolean infected = random().baseInfectionRate;
+		if infected == true {
+			this.status = "EXPOSED";
+			break;
+			return step();
+		}
+	}
+	return;
+}
